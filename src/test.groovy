a461f03917fed1f314ef986a4cae223f3817ef18
Chad James <ChadJames@columbus.rr.com>
2013-11-10 17:46:39 -0500

    test commit 1

---
+++ src/test.groovy
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: chad
+ * Date: 11/10/13
+ * Time: 5:45 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class test {
+    def print(){
+        println 'this the first test'
+    }
+}
